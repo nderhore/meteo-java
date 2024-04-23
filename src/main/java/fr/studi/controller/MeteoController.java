68be5e9b803498dfae18ab0699575e1dffebd932
nderhore <nathan@derhore.fr>
2024-04-23 19:29:37 +0200

    feat(btn_meteo): call function on click

---
+++ src/main/java/fr/studi/controller/MeteoController.java
@@ -0,0 +1,10 @@
+package fr.studi.controller;
+
+import javafx.event.ActionEvent;
+
+public class MeteoController {
+
+    public void displayText(ActionEvent actionEvent) {
+        System.out.println("Bonjour !");
+    }
+}
